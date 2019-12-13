1d019710347dcd336aeefda6663adf51d087a61d
Placide IRANDORA <placideirandora@gmail.com>
2019-12-13 16:11:59 +0200

    Add a model for creating conferences table

---
+++ Models/Conference.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace api_with_asp.net.Models {
+    public class Conference {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TicketPrice { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+}
