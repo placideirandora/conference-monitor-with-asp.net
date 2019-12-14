1d92ac88f6b3112224e2ef335a38f0014490c20b
Placide IRANDORA <placideirandora@gmail.com>
2019-12-14 10:28:43 +0200

    Add more columns

--- Models/Conference.cs
+++ Models/Conference.cs
@@ -3,6 +3,9 @@ namespace api_with_asp.net.Models
     public class Conference {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Theme { get; set; }
+        public string Description { get; set; }
+        public string Organizer { get; set; }
         public decimal TicketPrice { get; set; }
     }
 }
