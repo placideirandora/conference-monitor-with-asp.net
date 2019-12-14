4665430f90df9e40d3b497ee5fffc68a671008d9
Placide IRANDORA <placideirandora@gmail.com>
2019-12-14 08:50:59 +0200

    Get rid of no longer needed model

--- Models/Session.cs
+++
@@ -1,8 +0,0 @@
-namespace api_with_asp.net.Models {
-    public class Session {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int ConferenceId { get; set; }
-        public Conference Conference { get; set; }
-    }
-}
