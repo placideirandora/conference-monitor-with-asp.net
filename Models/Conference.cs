84e8c9c55ad7e2e3af6892855451a0ad00c4f942
Placide IRANDORA <placideirandora@gmail.com>
2019-12-14 09:11:02 +0200

    Get rid of no needed namespace

--- Models/Conference.cs
+++ Models/Conference.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
-
-namespace api_with_asp.net.Models {
+namespace api_with_asp.net.Models
+{
     public class Conference {
         public int Id { get; set; }
         public string Name { get; set; }
