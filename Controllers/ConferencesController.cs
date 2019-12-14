3ab5c09eda835363e831ddc81e0454bcc40be197
Placide IRANDORA <placideirandora@gmail.com>
2019-12-14 02:16:18 +0200

    Improve the controller by defining everything explicitly

--- Controllers/ConferencesController.cs
+++ Controllers/ConferencesController.cs
@@ -3,8 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace api_with_asp.net.Controllers {
     [Route("api/[controller]")]
     public class ConferencesController : ControllerBase {
-        public object Get() {
-            return new { Id = 1, ConferenceName = "DefCon 2020" };
+        [HttpGet]
+        public IActionResult GetConferences() {
+            return Ok(new { Id = 1, ConferenceName = "DefCon 2020" });
         }
 
     }
