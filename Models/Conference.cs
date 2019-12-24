61ce7f5a42827a8f95128b1a1be2b67d9308acfb
Placide IRANDORA <placideirandora@gmail.com>
2019-12-24 23:44:13 +0200

    Validate the model

--- Models/Conference.cs
+++ Models/Conference.cs
@@ -1,15 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ConferenceMonitorApi.Models
 {   
-    // Create a model for conferences table
+    // Create and validate the model for conferences table
+    [Table("Conferences")]
     public class Conference {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Conference Name is required")]
+        [StringLength(50, ErrorMessage = "Conference Name cannot be longer than 50 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Conference Theme is required")]
+        [StringLength(100, ErrorMessage = "Conference Theme cannot be longer than 100 characters")]
         public string Theme { get; set; }
+
+        [Required(ErrorMessage = "Conference Location is required")]
+        [StringLength(50, ErrorMessage = "Conference location cannot be longer than 30 characters")]
         public string Location { get; set; }
+
+        [Required(ErrorMessage = "Conference Start Date and Time are required")]
         public string StartDateAndTime { get; set; }
+
+        [Required(ErrorMessage = "Conference End Date and Time are required")]
         public string EndDateAndTime { get; set; }
+
+        [Required(ErrorMessage = "Conference Description is required")]
+        [StringLength(400, ErrorMessage = "Conference Description cannot be longer than 200 characters")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Conference Organizer is required")]
+        [StringLength(30, ErrorMessage = "Conference Organizer cannot be longer than 30 characters")]
         public string Organizer { get; set; }
+
         public decimal TicketPrice { get; set; }
     }
 }
