622bb2c07140575057bc1004cb2778c128b3fd09
Placide IRANDORA <placideirandora@gmail.com>
2019-12-25 20:52:05 +0200

    Define methods for the User Repository functionalities

---
+++ Data/IUserRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConferenceMonitorApi.Data
+{   
+    // Define an interface for the repository functionalities
+    public interface IUserRepository
+    {
+        Task<List<T>> FindAllAsync<T>() where T : class;
+        Task<T> FindByIdAsync<T>(int id) where T : class;
+        Task CreateAsync<T>(T entity) where T : class;
+        Task UpdateAsync<T>(int id, T entity) where T : class;
+        Task DeleteAsync<T>(T entity) where T : class;
+    }
+}
