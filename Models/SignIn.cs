ce5b99b641dc397998ae2b9f1ef1674ca59d04d1
Placide IRANDORA <placideirandora@gmail.com>
2019-12-26 01:33:07 +0200

    Implement the Interface by validating Login credentials

---
+++ Models/SignIn.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceMonitorApi.Models {
+    public class SignIn : ISignIn {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is required")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Invalid Email Address. (ex: someone@email.com)")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
