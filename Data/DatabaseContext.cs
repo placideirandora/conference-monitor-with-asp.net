a82191e67ccfccabbd1c1e2e341228df94916d70
Placide IRANDORA <placideirandora@gmail.com>
2019-12-25 16:12:19 +0200

    Create Users table

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -2,8 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ConferenceMonitorApi.Models {
     public class DatabaseContext : DbContext {
-        // Create table
+        // Create tables
         public DbSet<Conference> Conferences { get; set; }
+        public DbSet<User> Users { get; set; }
 
         // Override database configuration 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
